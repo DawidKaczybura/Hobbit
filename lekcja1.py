dcbfc8db2c8adc7ef95362c20718a6dbc7a95a53
systemm4tic <kaczybura.dawid@gmail.com>
2021-11-18 22:57:41 +0100

    lekcja1 i lekcja2 utworzone

---
+++ lekcja1.py
@@ -0,0 +1,21 @@
+print("Siema")
+
+a1 = 5
+a2 = 6
+
+print(a1+a2)
+
+a3 = a1+a2
+print(a3)
+
+oceny = [1,3,5,6,5]
+sumaocen = sum(oceny)
+print(sumaocen)
+
+print(sum(oceny))
+
+srednia = sumaocen/len(oceny)
+print(srednia)
+
+
+print(oceny.count(5))
