fe6d22c9b45b6d2fd28292a6f1572d955ddbdc5b
Hobbit013 <69675806+Hobbit013@users.noreply.github.com>
2021-11-28 16:51:24 +0100

    dodanie: czy jeszcze raz?

--- lekcja2.py
+++ lekcja2.py
@@ -1,32 +1,30 @@
 from random import randrange
 
 wylosowanaLiczba = randrange(1,11)
-#print(wylosowanaLiczba)
+
 graAktywna = True
 
 licznikProb = 0
 
-
 while graAktywna == True:
     
+    print(wylosowanaLiczba)
     licznikProb = licznikProb + 1
     liczbaGracza = int(input("Podaj liczbe:"))
     
     if(liczbaGracza == wylosowanaLiczba):
         
         print("Wygrales")
-        graAktywna = False
+        
+        print ("GRATULACJE KONIEC GRY")
+        print ("Zgadles w " + str(licznikProb) + " probach")
+        jeszczeRaz = int(input("Czy chcesz zagrać jeszcze raz YES(1) or NO(2) ???"))
+        if(jeszczeRaz == 1):
+            #graAktywna = True #dlaczego to tu musi być?? jak było tylko to..
+            licznikProb = 0
+            wylosowanaLiczba = randrange(1,11)
+        else:
+            print("Dzięki za grę, mam nadzieję że znów kiedyś pogramy")
+            graAktywna = False
     else: 
         print("Przegrales, spróbuj ponownie.")
-   
-
-
-
-
-print ("GRATULACJE KONIEC GRY")
-print ("Zgadles w " + str(licznikProb) + " probach")
-jeszczeRaz = int(input("Czy chcesz zagrać jeszcze raz Y or N ???"))
-if(jeszczeRaz == 1):
-    graAktywna = True
-else:
-    print("Dzięki za grę, mam nadzieję że znów kiedyś pogramy")
