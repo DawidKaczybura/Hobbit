679247089f71654319ee169ecfd88697afcf635d
Hobbit013 <69675806+Hobbit013@users.noreply.github.com>
2021-11-30 15:09:45 +0100

    a

--- test2.py
+++ test2.py
@@ -11,12 +11,12 @@ while graAktywna == True:
     iloscProb = iloscProb + 1
     liczbaGracza = int(input("Podaj liczbę: "))
 
-    if liczbaGracza == losowaLiczba:
+    if (liczbaGracza == losowaLiczba):
         print ("Gratulacje wygrales ! ! !")
         print ("Zgadłeś w "  + str(iloscProb) + " próbach")
         
         jeszczeRaz = int(input("Chcesz zagrać jeszcze raz? YES(1) NO(2): "))
-        if jeszczeRaz == 1:
+        if (jeszczeRaz == 1):
             losowaLiczba = randrange(1,11)
             iloscProb = 0
 
