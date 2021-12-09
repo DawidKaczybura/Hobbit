c4f23af63dbe5e1647f92f49aee922ed0d0db3ff
Hobbit013 <69675806+Hobbit013@users.noreply.github.com>
2021-12-09 10:34:51 +0100

    a

--- test3.py
+++ test3.py
@@ -38,7 +38,7 @@ while status == True:
         poprawne = poprawne + 1
     if (liczba6 == liczbaGracza6):
         poprawne = poprawne + 1
-        
+        #czy można od tąd pominąć if ?? albo zrobić tak żeby "jedno" było i pod if i else?
         print("Wylosowane liczby: " + str(losowanie))
         print("Twoje liczby:      " + str(liczbyGraczaAll))
         print("Zgadłeś " + str(poprawne) + " liczby")
