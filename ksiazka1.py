dd7c26042457e499ad9724bcd78d2b2f12aa2c95
Hobbit013 <69675806+Hobbit013@users.noreply.github.com>
2021-12-13 22:01:25 +0100

    a

---
+++ ksiazka1.py
@@ -0,0 +1,13 @@
+name = "jak kowalski"
+print(name.title())
+
+name = "Jan Kowalski"
+print(name.upper())
+print(name.lower())
+
+first_name = "Michael"
+last_name = "Hueniger"
+full_name = first_name + " " + last_name
+print(full_name)
+message = "Witaj " + full_name + "!"
+print(message)
