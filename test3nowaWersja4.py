9e4175ff7a98b873d9b2fcd85dc78581204d4e90
Hobbit013 <69675806+Hobbit013@users.noreply.github.com>
2021-12-24 22:02:48 +0100

    a

--- test3nowaWersja4.py
+++ test3nowaWersja4.py
@@ -6,23 +6,25 @@ proby = 0
 while status == True:
     haslo = []
     
-    for x in range(6):
+    for x in range(4):
         haslo.append(randrange(0,10)) # jak daje samo 10 to też działa, dlaczego??
     
     hack = 0
-    while hack != haslo:
+    while (hack != haslo):
         hack = random.randint(0,9999)
         print(hack)
 
-        jeszczeRaz = int(input("Czy złamać hasło jeszcze jaz ??? YES(1) NO(2): "))
-        if(jeszczeRaz == 1):
-            status = True
-        else:
-            ststus = False
+    print("Ustawione hasło: " + str(haslo))
+    print("Złamane hasło: " + str(hack))
+
+    jeszczeRaz = int(input("Czy złamać hasło jeszcze jaz ??? YES(1) NO(2): "))
+    if(jeszczeRaz == 1):
+        status = True
+    else:
+        ststus = False
 
     
     
     
-    print("Ustawione hasło: " + str(haslo))
-    print("Złamane hasło: " + str(hack))
+    
     
