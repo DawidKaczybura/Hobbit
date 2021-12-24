6cde07402c35e7ead07979ec41fcb102aa348a53
Hobbit013 <69675806+Hobbit013@users.noreply.github.com>
2021-12-24 22:10:33 +0100

    a

--- test3nowaWersja4.py
+++ test3nowaWersja4.py
@@ -8,11 +8,13 @@ while status == True:
     
     for x in range(4):
         haslo.append(randrange(0,10)) # jak daje samo 10 to też działa, dlaczego??
-    
+    print(haslo)
+    hasloPo = (haslo[1]+haslo[2]+haslo[3]+haslo[4])
+    print(hasloPo)
     hack = 0
-    while (hack != haslo):
-        hack = random.randint(0,9999)
-        print(hack)
+   # while (hack != haslo):
+       # hack = random.randint(0,9999)
+      #  print(hack)
 
     print("Ustawione hasło: " + str(haslo))
     print("Złamane hasło: " + str(hack))
