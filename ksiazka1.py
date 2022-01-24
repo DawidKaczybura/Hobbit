0c72a3c66781e0ac90abcacadf938c3f661f593e
Hobbit013 <69675806+Hobbit013@users.noreply.github.com>
2022-01-24 15:42:27 +0100

    a

--- ksiazka1.py
+++ ksiazka1.py
@@ -1,13 +1,52 @@
 name = "jak kowalski"
 print(name.title())
+# Jak Kowalski
 
 name = "Jan Kowalski"
 print(name.upper())
+# JAN KOWALSKI
 print(name.lower())
+# jan kowalski
 
-first_name = "Michael"
-last_name = "Hueniger"
+first_name = "michael"
+last_name = "hueniger"
 full_name = first_name + " " + last_name
-print(full_name)
-message = "Witaj " + full_name + "!"
+print("Witaj, " + full_name.title() + "!")
+# Witaj, Michael Hueniger!
+message = "Witaj, " + full_name.title() + "!"
 print(message)
+# Witaj, Michael Hueniger!
+
+print("Python")
+# Python
+print("\tPython")
+#         Python
+
+print("Języki:\nPyton\nC\nJavaScript")
+# Języki:
+# Pyton
+# C
+# JavaScript
+
+print("Języki:\n\tPyton\n\tC\n\tJavaScript")
+# Języki:
+#         Pyton
+#         C
+#         JavaScript
+
+favorite_language = 'python '
+print(favorite_language)
+# 'python '
+print(favorite_language.rstrip())
+# 'python'
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+# 'python'
+
+favorite_language = ' python '
+print(favorite_language.rstrip())
+# ' python'
+print(favorite_language.lstrip())
+# 'python '
+print(favorite_language.strip())
+# 'python'
